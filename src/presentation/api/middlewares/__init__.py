6e3b5c3a0a9b25501d4a6dabb9633d67e95ff810
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-02-17 19:05:50 +0100

    CORS

--- src/presentation/api/middlewares/__init__.py
+++ src/presentation/api/middlewares/__init__.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from src.presentation.api.middlewares.app_error import AppErrorHandlerMiddleware
 
 
 def setup_middlewares(app: FastAPI):
     app.add_middleware(AppErrorHandlerMiddleware)
+
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=["*"],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
