969f7df5fcb7a4f58a0b8d988f8800e275faf888
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-04-03 12:35:22 +0200

    Hot fix

--- src/application/project/dto.py
+++ src/application/project/dto.py
@@ -7,16 +7,16 @@ class ProjectDTO(DTO):
     id: int
     name: str = Field(max_length=255)
     description: str | None = Field(default=None, max_length=512)
-    content: dict | None = Field(default=None)
+    content: dict | list | None = Field(default=None)
 
 
 class UploadProjectDTO(DTO):
     name: str = Field(max_length=255)
     description: str | None = Field(default=None, max_length=512)
-    content: dict | None = Field(default=None)
+    content: dict | list | None = Field(default=None)
 
 
 class UpdateProjectDTO(DTO):
     name: str | None = Field(default=None, max_length=255)
     description: str | None = Field(default=None, max_length=512)
-    content: dict | None = Field(default=None)
+    content: dict | list | None = Field(default=None)
