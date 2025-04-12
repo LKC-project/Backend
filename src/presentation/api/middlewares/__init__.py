54d76e67edb0df40e0a78140fa1aa4378d08cb28
Жеревчук Сергій <Foxtrotserega@gmail.com>
2025-04-12 20:35:19 +0200

    CORS

--- src/presentation/api/middlewares/__init__.py
+++ src/presentation/api/middlewares/__init__.py
@@ -13,7 +13,7 @@ def setup_middlewares(app: FastAPI):
                        "http://localhost:5174",
                        "http://127.0.0.1:5173",
                        "http://127.0.0.1:5174",
-                       "https://gridium.xyz/"],
+                       "https://gridium.xyz"],
         allow_credentials=True,
         allow_methods=["*"],
         allow_headers=["*"],
